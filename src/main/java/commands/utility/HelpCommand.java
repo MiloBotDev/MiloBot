bb7b48635f7a7f2781bf6d4ea469c274d3060741
Ruben <rubeneekhof@gmail.com>
2022-05-31 17:32:11 +0200

    Added a basic paginator for the help command.

--- src/main/java/commands/utility/HelpCommand.java
+++ src/main/java/commands/utility/HelpCommand.java
@@ -3,19 +3,23 @@ package commands.utility;
 import commands.Command;
 import commands.CommandLoader;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import utility.EmbedUtils;
 
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * The Help command.
  * Shows the user an overview of every command, or detailed information on a specific command.
  * @author Ruben Eekhof - rubeneekhof@gmail.com
  */
-public class HelpCommand extends Command implements UtilityCommand{
+public class HelpCommand extends Command implements UtilityCommand {
 
     public HelpCommand() {
         this.commandName = "help";
@@ -27,6 +31,7 @@ public class HelpCommand extends Command implements UtilityCommand{
     @Override
     public void execute(@NotNull MessageReceivedEvent event, List<String> args) {
         if(checkForFlags(event, args, commandName, commandDescription, commandArgs, aliases, flags, cooldown)) {return;}
+
         if(args.size() > 0) {
             CommandLoader.commandList.forEach((key, value) -> {
                 if (key.contains(args.get(0).toLowerCase(Locale.ROOT))) {
@@ -36,17 +41,52 @@ public class HelpCommand extends Command implements UtilityCommand{
             });
         } else {
             String consumerName = event.getAuthor().getName();
+
+            EmbedBuilder categoryEmbed = new EmbedBuilder();
+            EmbedUtils.styleEmbed(event, categoryEmbed);
+            categoryEmbed.setTitle("Categories 📝");
+            categoryEmbed.setDescription("Click each categories respective emoji to see their commands.");
+            categoryEmbed.addField("Utility 🔨", UtilityCommand.description, true);
+
             EmbedBuilder utilityEmbed = new EmbedBuilder();
             EmbedUtils.styleEmbed(event, utilityEmbed);
-            utilityEmbed.setTitle("Utility Commands");
+            utilityEmbed.setTitle("Utility Commands 🔨");
             CommandLoader.commandList.forEach((key, value) -> {
                 if(value instanceof UtilityCommand) {
-                    utilityEmbed.addField(value.commandName, value.commandDescription, true);
+                    utilityEmbed.addField(String.format("!%s", value.commandName), value.commandDescription, true);
                 }
             });
+            Map<String, EmbedBuilder> embedAsEmoji = new HashMap<>();
+            embedAsEmoji.put("📝", categoryEmbed);
+            embedAsEmoji.put("🔨", utilityEmbed);
 
             event.getChannel().sendTyping().queue();
-            event.getChannel().sendMessageEmbeds(utilityEmbed.build()).queue(EmbedUtils.deleteEmbedButton(event, consumerName));
+            event.getChannel().sendMessageEmbeds(categoryEmbed.build()).queue(
+                    message -> {
+                        message.addReaction("📝").queue();
+                        message.addReaction("🔨").queue();
+                        message.addReaction("❌").queue();
+                        ListenerAdapter listener = new ListenerAdapter() {
+                            @Override
+                            public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event1) {
+                                String messageId = event1.getMessageId();
+                                if (Objects.requireNonNull(event1.getUser()).getName().equals(consumerName)
+                                        && message.getId().equals(messageId)) {
+                                    String asReactionCode = event1.getReactionEmote().getAsReactionCode();
+                                    if(asReactionCode.equals("❌")) {
+                                        event.getChannel().deleteMessageById(messageId).queue();
+                                        event.getJDA().removeEventListener(this);
+                                    } else {
+                                        message.removeReaction(asReactionCode, event1.getUser()).queue();
+                                        message.editMessageEmbeds(embedAsEmoji.get(asReactionCode).build()).queue();
+                                    }
+                                }
+                            }
+                        };
+                        message.getJDA().getRateLimitPool().schedule(() -> event.getJDA().removeEventListener(listener),
+                                1, TimeUnit.MINUTES);
+                        message.getJDA().addEventListener(listener);
+                    });
         }
     }
 
--- src/main/java/commands/utility/UtilityCommand.java
+++ src/main/java/commands/utility/UtilityCommand.java
@@ -1,4 +1,6 @@
 package commands.utility;
 
 public interface UtilityCommand {
+
+    String description = "Commands for general use.";
 }
