8b1f4f6c6ff208f44404ebe8d86b2455f7e0095d
Gabor Szita <gaborszita88@gmail.com>
2022-08-08 10:34:11 -0700

    Add DIRECT_MESSAGES intent
    Added checks to onMessageReceived event handlers to check if message
    is from a text channel (guild channel), because now this event fires
    also when private messages are received.

--- src/main/java/commands/CommandHandler.java
+++ src/main/java/commands/CommandHandler.java
@@ -4,6 +4,7 @@ import database.DatabaseManager;
 import database.queries.DailiesTableQueries;
 import database.queries.PrefixTableQueries;
 import database.queries.UsersTableQueries;
+import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -41,6 +42,10 @@ public class CommandHandler extends ListenerAdapter {
 
 	@Override
 	public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+		if (event.getChannelType() != ChannelType.TEXT) {
+			return;
+		}
+
 		// ignore messages from other bots or itself
 		if (event.getMessage().getAuthor().isBot()) {
 			return;
