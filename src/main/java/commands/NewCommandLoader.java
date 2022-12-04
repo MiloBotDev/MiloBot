bfbfb1bbabf716c136790724365947c96471003a
Gabor Szita <gaborszita88@gmail.com>
2022-12-04 00:50:38 -0800

    Add NewCommandLoader

---
+++ src/main/java/commands/NewCommandLoader.java
@@ -0,0 +1,10 @@
+package commands;
+
+import net.dv8tion.jda.api.JDA;
+
+public class NewCommandLoader {
+
+    public static void loadAllCommands(JDA jda) {
+        // TODO implement this
+    }
+}
