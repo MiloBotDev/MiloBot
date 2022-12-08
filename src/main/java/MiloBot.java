0f738074da56ab7df95d3db04827831f0fa9d6dc
Gabor Szita <gaborszita88@gmail.com>
2022-12-07 22:51:57 -0800

    Update main class to use new command loader

--- src/main/java/MiloBot.java
+++ src/main/java/MiloBot.java
@@ -1,13 +1,4 @@
-import commands.CommandLoader;
-import commands.GuildPrefixManager;
-import events.OnReadyEvent;
-import events.guild.OnGuildJoin;
-import events.guild.OnGuildLeave;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import utility.Config;
+import tk.milobot.BotInitializer;
 
 import javax.security.auth.login.LoginException;
 
@@ -16,21 +7,8 @@ import javax.security.auth.login.LoginException;
  */
 public class MiloBot {
 
-    public static void main(String[] args) throws LoginException, InterruptedException {
-        Config config = Config.getInstance();
-
-        JDA bot = null;
-        bot = JDABuilder.createDefault(config.getBotToken(),
-                        GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_EMOJIS, GatewayIntent.GUILD_VOICE_STATES,
-                        GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_TYPING, GatewayIntent.DIRECT_MESSAGE_TYPING,
-                        GatewayIntent.DIRECT_MESSAGE_TYPING, GatewayIntent.DIRECT_MESSAGE_REACTIONS, GatewayIntent.GUILD_MESSAGE_REACTIONS,
-                        GatewayIntent.DIRECT_MESSAGES)
-                .setActivity(Activity.watching("Morbius"))
-                .addEventListeners(new OnGuildJoin(), new OnGuildLeave(),
-                        new OnReadyEvent(), GuildPrefixManager.getInstance().getEventListener())
-                .build().awaitReady();
-
-        CommandLoader.loadAllCommands(bot);
+    public static void main(String[] args) {
+        BotInitializer.initialize();
 
         // uncomment this line to generate documentation for all commands
         // CommandHandler.generateDocumentation();
