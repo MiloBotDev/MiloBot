e6874256afc0fa339111fbb587ba0bd4a0218f86
Gabor Szita <gaborszita88@gmail.com>
2022-12-07 22:06:31 -0800

    Change NewCommandLoader to new structure

--- src/main/java/commands/NewCommandLoader.java
+++ src/main/java/commands/NewCommandLoader.java
@@ -1,21 +1,11 @@
 package commands;
 
-import commands.games.blackjack.NewBlackjackCmd;
-import commands.games.blackjack.NewBlackjackPlayCmd;
-import net.dv8tion.jda.api.JDA;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class NewCommandLoader {
 
-    public static void loadAllCommands(JDA jda) {
-        NewCommandHandler handler = new NewCommandHandler(jda);
-
-        ExecutorService blackjackServiice = Executors.newSingleThreadExecutor();
-        handler.registerCommand(new NewBlackjackCmd(blackjackServiice)
-                .addSubCommand(new NewBlackjackPlayCmd(blackjackServiice)));
-
-        handler.initialize();
+    public static void initialize() {
+        // put every command initializer call here
+        // keep this method as short as possible, you should only have one-liners here, like SomeClass.initialize()
+        // or SomeClass.getInstance().initialize()
+        // if you need more than one line to initialize something, it means that it should be in its own class
     }
 }
