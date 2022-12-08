dd4656d42fc2b0cd9e07f02224a5f3eb42ead31e
Gabor Szita <gaborszita88@gmail.com>
2022-12-07 22:47:10 -0800

    Add blackjack command loader

--- src/main/java/commands/NewCommandLoader.java
+++ src/main/java/commands/NewCommandLoader.java
@@ -1,5 +1,7 @@
 package commands;
 
+import commands.loaders.BlackjackLoader;
+
 public class NewCommandLoader {
 
     public static void initialize() {
@@ -7,5 +9,7 @@ public class NewCommandLoader {
         // keep this method as short as possible, you should only have one-liners here, like SomeClass.load()
         // or SomeClass.getInstance().load()
         // if you need more than one line to initialize something, it means that it should be in its own class
+
+        BlackjackLoader.load();
     }
 }
---
+++ src/main/java/commands/games/blackjack/NewBlackjackCmd.java
@@ -0,0 +1,54 @@
+package commands.games.blackjack;
+
+import commands.newcommand.ParentCommand;
+import commands.newcommand.extensions.Aliases;
+import commands.newcommand.extensions.DefaultFlags;
+import commands.newcommand.extensions.DefaultSlashParentCommand;
+import commands.newcommand.extensions.DefaultTextParentCommand;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.interactions.commands.build.BaseCommand;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+
+public class NewBlackjackCmd extends ParentCommand implements DefaultTextParentCommand, DefaultSlashParentCommand,
+        Aliases, DefaultFlags {
+    private final ExecutorService executorService;
+
+    public NewBlackjackCmd(ExecutorService executorService) {
+        this.executorService = executorService;
+        /*this.commandName = "blackjack";
+        this.commandDescription = "Blackjack brought to discord.";
+        this.aliases = new String[]{"bj"};
+        this.subCommands.add(new BlackjackPlayCmd());
+        this.subCommands.add(new BlackjackStatsCmd());
+        this.subCommands.add(new BlackjackInfoCmd());
+        this.subCommands.add(new BlackjackLeaderboardCmd());
+        this.allowedChannelTypes.add(ChannelType.TEXT);
+        this.allowedChannelTypes.add(ChannelType.PRIVATE);
+        this.subCommands.forEach(subCmd -> subCmd.parentCommandName = this.commandName);
+        this.slashCommandData = new CommandData(this.commandName, this.commandDescription);*/
+    }
+
+    @Override
+    public BaseCommand<?> getCommandData() {
+        return new CommandData("blackjack", "Blackjack brought to discord.");
+    }
+
+    @Override
+    public Set<ChannelType> getAllowedChannelTypes() {
+        return Set.of(ChannelType.TEXT, ChannelType.PRIVATE);
+    }
+
+    @Override
+    public ExecutorService getExecutorService() {
+        return executorService;
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return List.of("bj");
+    }
+}
