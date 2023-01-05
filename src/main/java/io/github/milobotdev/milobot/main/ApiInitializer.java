6316353511416c4847cec37ad4d8213e6d65fdef
Gabor Szita <gaborszita88@gmail.com>
2023-01-05 15:05:12 +0100

    fully set up jersey and grizzly

--- src/main/java/io/github/milobotdev/milobot/main/ApiInitializer.java
+++ src/main/java/io/github/milobotdev/milobot/main/ApiInitializer.java
@@ -3,12 +3,17 @@ package io.github.milobotdev.milobot.main;
 import io.github.milobotdev.milobot.api.App;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.model.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ApiInitializer {
@@ -17,6 +22,7 @@ public class ApiInitializer {
     public static void initialize() {
         ResourceConfig config = new ResourceConfig();
         config.register(App.class);
+        config.setProperties(Map.of("jersey.config.server.wadl.disableWadl", "true"));
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create("http://localhost:8080/"), config);
         try {
             server.start();
--- src/main/java/io/github/milobotdev/milobot/main/MiloBot.java
+++ src/main/java/io/github/milobotdev/milobot/main/MiloBot.java
@@ -1,11 +1,17 @@
 package io.github.milobotdev.milobot.main;
 
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
 /**
  * The Main class from where the bot is started.
  */
 public class MiloBot {
 
     public static void main(String[] args) {
+        // Glassfish Jersey and Grizzly (for API) use java.util.logging, so we need to redirect java.util.logging to slf4j
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+
         BotInitializer.initialize();
         ApiInitializer.initialize();
 
