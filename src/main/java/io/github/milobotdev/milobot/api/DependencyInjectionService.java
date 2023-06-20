4c7b2543b52a727b596204a472fa803c5255b738
Gabor Szita <gaborszita88@gmail.com>
2023-06-20 18:16:29 +0200

    PostConstruct dependency injection

--- src/main/java/io/github/milobotdev/milobot/api/DependencyInjectionService.java
+++ src/main/java/io/github/milobotdev/milobot/api/DependencyInjectionService.java
@@ -2,9 +2,10 @@ package io.github.milobotdev.milobot.api;
 
 
 import jakarta.inject.Singleton;
+import org.glassfish.hk2.api.PostConstruct;
 import org.jvnet.hk2.annotations.Service;
 
-public class DependencyInjectionService {
+public class DependencyInjectionService implements PostConstruct {
     public String getstg() {
         return "stg";
     }
@@ -12,5 +13,10 @@ public class DependencyInjectionService {
     private int num = 0;
     public void num() {
         System.out.println(num++);
+    }
+
+    @Override
+    public void postConstruct() {
+        System.out.println("Helloagain");
     }
 }
